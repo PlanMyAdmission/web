1490ceb6faab2afaa9ed8807bb060248af1f0394
Arnav Gupta <info@arnavgupta.net>
2026-05-13 12:58:25 +0530

    feat: added chat service

--- components/ai-chatbot/hooks/useChatbotSession.js
+++ components/ai-chatbot/hooks/useChatbotSession.js
@@ -95,7 +95,7 @@ const useChatbotSession = () => {
 
     clientRef.current = client;
     return () => {
-      client.close();
+      client?.close();
       clientRef.current = null;
     };
   }, []);
